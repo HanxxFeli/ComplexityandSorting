3be398489ff0cdb5d66c70ab692744c3bdc31393
celinesur <ciiellasurjanto@gmail.com>
2025-10-15 15:30:32 -0600

    added and tested the prism subclasses (square, triangle, penta, octa)

--- assignment1StartingCode/src/shapes/TriangularPrism.java
+++ assignment1StartingCode/src/shapes/TriangularPrism.java
@@ -1,6 +1,24 @@
 package shapes;
 
-public class TriangularPrism
+public class TriangularPrism extends Shape 
 {
-
+	private double edgeLength;
+	
+	public TriangularPrism(double height, double edgeLength)
+	{
+		super(height);
+		this.edgeLength = edgeLength;
+	}
+	
+	@Override
+	public double calcBaseArea()
+	{
+		return (Math.sqrt(3) / 4) * Math.pow(edgeLength, 2);
+	}
+	
+	@Override
+	public double calcVolume() 
+	{
+		return calcBaseArea() * getHeight();
+	}
 }
