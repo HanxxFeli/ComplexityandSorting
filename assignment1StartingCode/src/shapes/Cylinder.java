70f14390f84544ef8e943e9369827ba7a0b9e9ef
yvnmnl <yvana.gaddao@edu.sait.ca>
2025-10-15 20:10:34 -0600

    created subclasses for cone, cylinder and pyramid

--- assignment1StartingCode/src/shapes/Cylinder.java
+++ assignment1StartingCode/src/shapes/Cylinder.java
@@ -1,6 +1,22 @@
 package shapes;
 
-public class Cylinder
+public class Cylinder extends Shape 
 {
+   private double radius;
 
+   public Cylinder(double height, double radius) 
+   {
+      super(height);
+      this.radius = radius;
+   }
+
+   public double calcBaseArea() 
+   {
+      return Math.PI * Math.pow(this.radius, 2.0);
+   }
+
+   public double calcVolume() 
+   {
+      return this.calcBaseArea() * this.getHeight();
+   }
 }
